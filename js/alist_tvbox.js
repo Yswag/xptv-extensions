e6258e6f5dfaef1c97e80985395d26ba53caef29
ykusu <yangswag0607@gmail.com>
2024-09-27 22:33:48 +0800

    update

--- js/alist_tvbox.js
+++ js/alist_tvbox.js
@@ -140,7 +140,11 @@ async function getPlayinfo(ext) {
     ext = argsify(ext)
     let url = ext.url
 
-    const { data } = await $fetch.get(url)
+    const { data } = await $fetch.get(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        },
+    })
 
     let playUrl = argsify(data).url
 
