51429b12bf41da8108ce4f5312e26e19ce474ebc
ykusu <yangswag0607@gmail.com>
2024-09-27 22:25:55 +0800

    update

--- js/alist_xiaoya.js
+++ js/alist_xiaoya.js
@@ -238,6 +238,7 @@ async function getPlayinfo(ext) {
         {
             headers: {
                 Authorization: token,
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
             },
         }
     )
