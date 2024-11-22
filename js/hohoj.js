0aa3443e90067a912e93fd99654380e793658ceb
ykusu <yangswag0607@gmail.com>
2024-11-22 21:37:11 +0800

    update

--- js/hohoj.js
+++ js/hohoj.js
@@ -1,3 +1,4 @@
+// 來自群友:夢
 const cheerio = createCheerio()
 
 const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Mobile/15E148 Safari/604.1'
