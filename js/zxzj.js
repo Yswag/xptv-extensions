ccdff96c92a2bf30b50bdfc09c02b825058f605b
Yswag <yangswag0607@gmail.com>
2025-11-19 11:39:08 +0800

    update zxzj

--- js/zxzj.js
+++ js/zxzj.js
@@ -1,25 +1,52 @@
-// tv demo test
-// const { $html, argsify, jsonify, $fetch, $print, $cache } = require('../test/libs.js')
-
-const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+const cheerio = createCheerio()
+const UA =
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
 
 const appConfig = {
-    ver: 1,
+    ver: 20251119,
     title: '在线之家',
-    site: 'https://www.zxzja.com',
+    site: 'https://www.zxzjhd.com',
     tabs: [
         {
             name: '首页',
             ext: {
                 id: 0,
-                url: 'https://www.zxzja.com/',
             },
         },
         {
             name: '电影',
             ext: {
                 id: 1,
-                url: 'https://www.zxzja.com/list/1.html',
+            },
+        },
+        {
+            name: '美剧',
+            ext: {
+                id: 2,
+            },
+        },
+        {
+            name: '韩剧',
+            ext: {
+                id: 3,
+            },
+        },
+        {
+            name: '日剧',
+            ext: {
+                id: 4,
+            },
+        },
+        {
+            name: '泰剧',
+            ext: {
+                id: 5,
+            },
+        },
+        {
+            name: '动漫',
+            ext: {
+                id: 6,
             },
         },
     ],
@@ -35,50 +62,50 @@ async function getCards(ext) {
     let id = ext.id
     let page = ext.page || 1
 
-    $print(`args: ${id} ${page}`)
-
-    var url = 'https://www.zxzja.com/'
-    if (id == 0 && page > 1) {
-        return jsonify({
-            list: cards,
-        })
-    }
-
-    if (id > 0) {
-        url = `https://www.zxzja.com/list/${id}.html`
-        if (page > 1) {
-            url = `https://www.zxzja.com/list/${id}-${page}.html`
-        }
-    }
-
-    // 发送请求
-    const { data } = await $fetch.get(url, {
-        headers: {
-            Referer: 'https://www.zxzja.com/',
-            'User-Agent': UA,
-        },
-    })
-
-    // 加载 HTML
-    const elems = $html.elements(data, 'a.lazyload')
-    elems.forEach((element) => {
-        const href = $html.attr(element, 'a', 'href')
-        const title = $html.attr(element, 'a', 'title')
-        const cover = $html.attr(element, 'a', 'data-original')
-        const subTitle = $html.text(element, '.text-right')
-
-        if (href.startsWith('/detail/')) {
-            cards.push({
-                vod_id: href.replace(/.*?\/detail\/(.*).html/g, '$1'),
-                vod_name: title,
-                vod_pic: cover,
-                vod_remarks: subTitle,
-                ext: {
-                    url: `${appConfig.site}${href}`,
-                },
+    try {
+        var url = `${appConfig.site}/`
+        if (id == 0 && page > 1) {
+            return jsonify({
+                list: cards,
             })
         }
-    })
+
+        if (id > 0) {
+            url = `${appConfig.site}/list/${id}.html`
+            if (page > 1) {
+                url = `${appConfig.site}/list/${id}-${page}.html`
+            }
+        }
+
+        const { data } = await $fetch.get(url, {
+            headers: {
+                // Referer: `${appConfig.site}/`,
+                'User-Agent': UA,
+            },
+        })
+
+        const $ = cheerio.load(data)
+        $('.stui-vodlist__box').each((_, element) => {
+            const href = $(element).find('.stui-vodlist__thumb').attr('href')
+            const title = $(element).find('.stui-vodlist__thumb').attr('title')
+            const cover = $(element).find('.stui-vodlist__thumb').attr('data-original')
+            const subTitle = $(element).find('.pic-text').find('.text-right').text()
+
+            if (href.startsWith('/detail/')) {
+                cards.push({
+                    vod_id: href.replace(/.*?\/detail\/(.*).html/g, '$1'),
+                    vod_name: title,
+                    vod_pic: cover,
+                    vod_remarks: subTitle,
+                    ext: {
+                        url: `${appConfig.site}${href}`,
+                    },
+                })
+            }
+        })
+    } catch (error) {
+        $print(error)
+    }
 
     return jsonify({
         list: cards,
@@ -93,18 +120,17 @@ async function getTracks(ext) {
     // 发送请求
     const { data } = await $fetch.get(url, {
         headers: {
-            Referer: 'https://www.zxzja.com/',
+            // Referer: 'https://www.zxzja.com/',
             'User-Agent': UA,
         },
     })
 
-    // 加载 HTML
-    const elems = $html.elements(data, '.stui-content__playlist > li > a')
+    const $ = cheerio.load(data)
 
     // 解析数据，例如提取标题
-    elems.forEach((element) => {
-        const href = $html.attr(element, 'a', 'href')
-        const name = $html.text(element, 'a')
+    $('.stui-content__playlist > li > a').each((_, each) => {
+        const href = $(each).attr('href')
+        const name = $(each).text()
         if (href && name && name !== '合集') {
             tracks.push({
                 name,
@@ -131,68 +157,106 @@ async function getPlayinfo(ext) {
     var plays = []
     let url = ext.url
 
-    // 发送请求
-    const { data } = await $fetch.get(url, {
-        headers: {
-            'User-Agent': UA,
-        },
-    })
-
-    const html = data.match(/r player_.*?=(.*?)</)[1]
-    const json = JSON.parse(html)
-
-    const playurl = json.url
-    const from = json.from
-    if (json.encrypt == '1') {
-        playurl = decodeURIComponent(url)
-    } else if (json.encrypt == '2') {
-        playurl = decodeURIComponent(Buffer.from(url, 'base64').toString('utf-8'))
-    }
-    $print(`playurl: ${playurl}`)
-    if (playurl.indexOf('m3u8') >= 0 || playurl.indexOf('mp4') >= 0) {
-        return jsonify({ urls: [playurl] })
-    } else if (from.indexOf('line3') >= 0 || from.indexOf('line5') >= 0) {
-        const { data } = await $fetch.get(playurl, {
+    try {
+        // 发送请求
+        const { data } = await $fetch.get(url, {
             headers: {
-                Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'zh-CN,zh;q=0.9',
-                Referer: 'https://www.zxzja.com/',
-                'Sec-Ch-Ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
-                'Sec-Ch-Ua-Mobile': '?0',
-                'Sec-Ch-Ua-Platform': '"macOS"',
-                'Sec-Fetch-Dest': 'iframe',
-                'Sec-Fetch-Mode': 'navigate',
-                'Sec-Fetch-Site': 'same-site',
-                'Upgrade-Insecure-Requests': '1',
                 'User-Agent': UA,
             },
         })
-        let resultv2 = data.match(/var result_v2 = {(.*?)};/)[1]
-        let code = JSON.parse('{' + resultv2 + '}')
-            .data.split('')
-            .reverse()
-        let temp = ''
-        for (let i = 0x0; i < code.length; i = i + 0x2) {
-            temp += String.fromCharCode(parseInt(code[i] + code[i + 0x1], 0x10))
+
+        const html = data.match(/r player_.*?=(.*?)</)[1]
+        const json = JSON.parse(html)
+
+        const playurl = json.url
+        const from = json.from
+        if (json.encrypt == '1') {
+            playurl = decodeURIComponent(url)
+        } else if (json.encrypt == '2') {
+            playurl = decodeURIComponent(Buffer.from(url, 'base64').toString('utf-8'))
         }
-        const purl = temp.substring(0x0, (temp.length - 0x7) / 0x2) + temp.substring((temp.length - 0x7) / 0x2 + 0x7)
-        $print('***在线之家purl =====>' + purl) // js_debug.log
-        return jsonify({ urls: [purl] })
+        $print(`playurl: ${playurl}`)
+        if (playurl.indexOf('m3u8') >= 0 || playurl.indexOf('mp4') >= 0) {
+            return jsonify({ urls: [playurl] })
+            // } else if (from.indexOf('line3') >= 0 || from.indexOf('line5') >= 0) {
+        } else {
+            const { data } = await $fetch.get(playurl, {
+                headers: {
+                    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept-Language': 'zh-CN,zh;q=0.9',
+                    Referer: `${appConfig.site}/`,
+                    'Sec-Ch-Ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+                    'Sec-Ch-Ua-Mobile': '?0',
+                    'Sec-Ch-Ua-Platform': '"macOS"',
+                    'Sec-Fetch-Dest': 'iframe',
+                    'Sec-Fetch-Mode': 'navigate',
+                    'Sec-Fetch-Site': 'same-site',
+                    'Upgrade-Insecure-Requests': '1',
+                    'User-Agent': UA,
+                },
+            })
+            let resultv2 = data.match(/var result_v2 = {(.*?)};/)[1]
+            let code = JSON.parse('{' + resultv2 + '}')
+                .data.split('')
+                .reverse()
+            let temp = ''
+            for (let i = 0x0; i < code.length; i = i + 0x2) {
+                temp += String.fromCharCode(parseInt(code[i] + code[i + 0x1], 0x10))
+            }
+            const purl =
+                temp.substring(0x0, (temp.length - 0x7) / 0x2) + temp.substring((temp.length - 0x7) / 0x2 + 0x7)
+            $print('***在线之家purl =====>' + purl)
+            return jsonify({ urls: [purl] })
+        }
+        return jsonify({ urls: [] })
+    } catch (error) {
+        $print(error)
     }
-    return jsonify({ urls: [] })
 }
 
 async function search(ext) {
     ext = argsify(ext)
     var cards = []
 
-    let text = ext.text // 搜索文本
+    let text = encodeURIComponent(ext.text)
     let page = ext.page || 1
+
+    if (page > 1) {
+        return jsonify({
+            list: cards,
+        })
+    }
+
+    const url = `${appConfig.site}/vodsearch/-------------.html?wd=${text}&submit=`
+    const { data } = await $fetch.get(url, {
+        headers: {
+            Referer: `${appConfig.site}`,
+            'User-Agent': UA,
+        },
+    })
+
+    const $ = cheerio.load(data)
+    $('a.lazyload').each((_, element) => {
+        const href = $(element).attr('href')
+        const title = $(element).attr('title')
+        const cover = $(element).attr('data-original')
+        const subTitle = $(element).find('.text-right').text()
+
+        if (href.startsWith('/detail/')) {
+            cards.push({
+                vod_id: href.replace(/.*?\/detail\/(.*).html/g, '$1'),
+                vod_name: title,
+                vod_pic: cover,
+                vod_remarks: subTitle,
+                ext: {
+                    url: `${appConfig.site}${href}`,
+                },
+            })
+        }
+    })
 
     return jsonify({
         list: cards,
     })
 }
-
-// module.exports = { getConfig, getCards, getTracks, getPlayinfo, search }
